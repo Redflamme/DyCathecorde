4cd3b2e19b3eb3e8a5da00caac7abf5b77bab713
DyCathecorde <dycathecorde@gmail.com>
2025-01-04 00:54:37 +0100

    nom

--- view/head.php
+++ view/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dycathecode</title>
+    <title><?=$dataSi['auteur']?></title>
     <link rel="stylesheet" href="web-fonts-with-css/css/fontawesome.min.css">
     <link rel="stylesheet" href="web-fonts-with-css/css/all.min.css">
     <link rel="stylesheet" href="web-fonts-with-css/css/all.css">
