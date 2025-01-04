9761f8259c8073d141266e711850abb636a749ff
DyCathecorde <dycathecorde@gmail.com>
2025-01-04 01:02:44 +0100

    css

--- view/head.php
+++ view/head.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="css/compossants.css">
     <link rel="stylesheet" href="css/mobile.css">
     <link rel="stylesheet" href="css/tablette.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/boostrap.css">
     <link rel="stylesheet" href="css/cdn.jsdelivr.net_npm_swiper@10.1.0_swiper-bundle.min.css">
     <!-- <script src="./js/jquery.js"></script> -->
     <link rel="shortcut icon" href="./asset/icon/logo.ico">
